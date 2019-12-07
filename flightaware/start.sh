7e0bdc938998977279c037204b8db7c0bacea009
Alex Rhea <alex.rhea@gmail.com>
2019-12-06 22:42:25 -0500

    fixing piaware config

--- flightaware/start.sh
+++ flightaware/start.sh
@@ -4,9 +4,9 @@
 /usr/bin/piaware-config receiver-type other
 /usr/bin/piaware-config use-gpsd no
 
-[[ ! -z ${PIAWARE_HOST} ]]             &&  /usr/bin/piaware-config receiver-host ${PIAWARE_HOST} || PIAWARE_HOST="dump1090"
-[[ ! -z ${PIAWARE_PORT} ]]             &&  /usr/bin/piaware-config receiver-port ${PIAWARE_PORT} || PIAWARE_PORT="30005"
-[[ ! -z ${PIAWARE_FEEDER_ID} ]]       && /usr/bin/piaware-config feeder-id ${PIAWARE_FEEDER_ID}
+[[ ! -z ${PIAWARE_HOST} ]]       &&  /usr/bin/piaware-config receiver-host ${PIAWARE_HOST} || PIAWARE_HOST="dump1090"
+[[ ! -z ${PIAWARE_PORT} ]]       &&  /usr/bin/piaware-config receiver-port ${PIAWARE_PORT} || PIAWARE_PORT="30005"
+[[ ! -z ${PIAWARE_FEEDER_ID} ]]  && /usr/bin/piaware-config feeder-id ${PIAWARE_FEEDER_ID}
 
 # Recommend adding this to the config
 [[ ! -z ${GAIN} ]]             && /usr/bin/piaware-config rtlsdr-gain ${GAIN} || GAIN="-10"
