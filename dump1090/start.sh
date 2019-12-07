01e6195e47c34590e09b35b70c578bde03357da5
Alex Rhea <alex.rhea@gmail.com>
2019-12-07 11:31:24 -0500

    fixing invoke.rc

--- dump1090/start.sh
+++ dump1090/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
 
+echo "Running Dump 1090"
 /usr/bin/dump1090-fa --net --lat $PIAWARE_LAT --lon $PIAWARE_LON
+
+echo "Running Light HTTPD"
+lighttpd -D -f etc/lighttpd/lighttpd.conf &
