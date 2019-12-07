1a0823af519a50066b5fab72ac4d973162e5a14a
Alex Rhea <alex.rhea@gmail.com>
2019-12-06 23:51:38 -0500

    updating configuration

--- dump1090/start.sh
+++ dump1090/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 /usr/bin/dump1090-fa --device-index 0 \
+                      --interactive \
                       --gain -10 \
                       --ppm 0 \
                       --max-range 360 \
                       --net \
+                      --net-bind-address \
                       --net-heartbeat 60 \
                       --net-ro-size 1000 \
                       --net-ro-interval 1 \
@@ -13,5 +15,5 @@
                       --net-sbs-port 30003 \
                       --net-bi-port 30004,30104 \
                       --net-bo-port 30005
-                      --lat $DUMP_LAT \
-                      --lon $DUMP_LON
+                      --lat $PIAWARE_LAT \
+                      --lon $PIAWARE_LON
