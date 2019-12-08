e7eef2dac92a8f5249fe61360c88a87b7b9327c5
Alex Rhea <alexrhea@amazon.com>
2019-12-07 20:00:19 -0500

    breaking out builds

---
+++ dump1090/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+# load the os version
+source /etc/os-release
+ARCH=$(uname -i)
+
+# create our directory
+mkdir -p /usr/src
+cd /usr/src
+
+# reset back to the src root
+cd /usr/src
+
+# clone the dump1090 repo
+git clone https://github.com/flightaware/dump1090.git
+
+# change into the repo
+cd dump1090
+# build the package
+dpkg-buildpackage -b
+
+if [ "$arch" == 'armv*' ]; then
+  dpkg -i ../dump1090-fa_3.7.2_arm.deb
+else
+  dpkg -i ../dump1090-fa_3.7.2_amd64.deb
+fi
---
+++ piaware/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+# load the os version
+source /etc/os-release
+ARCH=$(uname -i)
+
+# create our directory
+mkdir -p /usr/src
+cd /usr/src
+
+# clone the repository
+git clone https://github.com/flightaware/piaware_builder.git
+
+# change into the repo
+cd piaware_builder
+
+# run the init script
+./sensible-build.sh ${VERSION_CODENAME}
+
+# change into the staging directory
+cd ./package-${VERSION_CODENAME}
+
+# build the package
+dpkg-buildpackage -b
+
+# configure the package
+if [ "$arch" == 'armv*' ]; then
+  dpkg -i ../piaware_3.7.2_arm.deb
+else
+  dpkg -i ../piaware_3.7.2_amd64.deb
+fi
+
